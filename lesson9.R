8876e98d5331f2cdfce846d01dd5a1274c1ee27c
NTIVUGURUZWA AMOS <143718844+221002478@users.noreply.github.com>
2024-01-31 19:00:45 +0200

    Add files via upload

---
+++ lesson9.R
@@ -0,0 +1,28 @@
+## the codes below is for finding the mean of vector
+makeCacheMatrix <- function(x = matrix()) {
+  j <- NULL
+  set <- function(y){
+    x <<- y
+    j <<- NULL
+  }
+  get <- function()x
+  setInverse <- function(inverse) j <<- inverse
+  getInverse <- function() j 
+  list(set = set, get = get, 
+       setInverse = setInverse, 
+       getInverse = getInverse)
+}
+
+##the codes below is the one to find the inverse of the inverse of matrix
+cacheSolve <- function(x, ...) {
+  ## Return a matrix that is the inverse of 'x'
+  j <- x$getInverse()
+  if(!is.null(j)){
+    message("getting cached data")
+    return(j)
+  }
+  mat <- x$get()
+  j <- solve(mat,...)
+  x$setInverse(j)
+  j
+}
